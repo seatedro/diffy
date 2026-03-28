0f523edce2defe659219e865137a468695f27ef4
ro <git@seated.ro>
2026-03-28 16:02:34 -0700

    build: align tooling and docs with native workflow

--- devenv.nix
+++ devenv.nix
@@ -14,17 +14,9 @@
     pkgs.rr
     pkgs.strace
     pkgs.watchexec
-    pkgs.qt6.qtbase
-    pkgs.qt6.qtdeclarative
   ];
 
   enterShell = ''
-    qt_declarative_prefix="${pkgs.qt6.qtdeclarative}"
-    export DIFFY_REPO_ROOT="$PWD"
-    export QMAKE="${pkgs.lib.getExe' pkgs.qt6.qtbase "qmake"}"
-    export QT_ADDITIONAL_PACKAGES_PREFIX_PATH="$qt_declarative_prefix''${QT_ADDITIONAL_PACKAGES_PREFIX_PATH:+:''${QT_ADDITIONAL_PACKAGES_PREFIX_PATH}}"
-    export CXXFLAGS="-F$qt_declarative_prefix/lib -I$qt_declarative_prefix/include''${CXXFLAGS:+ ''${CXXFLAGS}}"
-    export RUSTFLAGS="-L framework=$qt_declarative_prefix/lib''${RUSTFLAGS:+ ''${RUSTFLAGS}}"
     echo "devenv ready: cargo build && cargo test"
     echo "run: cargo run"
     echo "debug ready: gdb ./target/debug/diffy | lldb ./target/debug/diffy | rr record ./target/debug/diffy"
